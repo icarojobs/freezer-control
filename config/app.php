170a045fdad6c434e8269cfce92216ac802d8c8e
Tio Jobs <icarojobsoficial@gmail.com>
2024-01-16 08:10:54 -0300

    chore: removed app panel

--- config/app.php
+++ config/app.php
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\Filament\AppPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('frontend.home');
+
+
+Route::get('/customer-administration', function () {
+    //return to_route('filament.auth.login');
+})->name('login');
