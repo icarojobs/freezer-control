77ae46412ad233a7bc322b623dd57a692fe6e6cf
Tio Jobs <icarojobsoficial@gmail.com>
2024-02-02 17:34:50 -0300

    refactor: refactor of CustomerResource class

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,6 +13,6 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
-        $this->assertTrue(true);
+//        $this->assertTrue(true);
     }
 }
