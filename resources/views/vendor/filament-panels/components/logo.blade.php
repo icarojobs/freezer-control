2ea1f2acbe3100e733f43afccc05fc00236bf952
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-02-17 12:46:25 -0300

    fix: add logo condition to login and registration area

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,10 +1,9 @@
-@if(Route::current()->getName() === 'filament.admin.auth.login')
+@if(Route::current()->getName() === 'filament.admin.auth.login' || Route::current()->getName() === 'filament.app.auth.register' || Route::current()->getName() === 'filament.app.auth.login')
     <img src="{{asset('images/brands/logo-h-white-720.png')}}"
          alt="{{config('app.name')}}"
          title="{{config('app.name')}}"
          width="310"
          class="mb-2"
-         style="filter: drop-shadow(rgb(10,20,10) 1px 2px 1px)"
     >
 @else
     <img src="{{asset('images/brands/logo-v-white-720.png')}}"
@@ -12,8 +11,7 @@
          title="{{config('app.name')}}"
          width="185"
          class="mb-2"
-         style="filter: drop-shadow(rgb(10,20,10) 1px 2px 1px)"
     >
- @endif
+@endif
 
 {{-- Precisa somente de uma condição para trocar as logos quando sistema esta dark e light e retirar o style --}}
