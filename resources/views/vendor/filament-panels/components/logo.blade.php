0c8827e417876b20a63634ffc53f22e84ab6d69f
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-02-19 09:50:05 -0300

    :bug: fix: Modified the path of the freezer control logo

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,12 +1,12 @@
 @if(Route::current()->getName() === 'filament.admin.auth.login' || Route::current()->getName() === 'filament.app.auth.register' || Route::current()->getName() === 'filament.app.auth.login')
-    <img src="{{asset('images/brands/logo-h-white-720.png')}}"
+    <img src="{{asset('brands/logo-h-white-720.png')}}"
          alt="{{config('app.name')}}"
          title="{{config('app.name')}}"
          width="310"
          class="mb-2"
     >
 @else
-    <img src="{{asset('images/brands/logo-v-white-720.png')}}"
+    <img src="{{asset('brands/logo-v-white-720.png')}}"
          alt="{{config('app.name')}}"
          title="{{config('app.name')}}"
          width="185"
@@ -14,4 +14,4 @@
     >
 @endif
 
-{{-- Precisa somente de uma condição para trocar as logos quando sistema esta dark e light e retirar o style --}}
+{{-- Falta add uma forma de trocar as imagens black ou white --}}
