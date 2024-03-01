0d240b52d034c8c981bd360d8b8af6bcff2d3a99
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-03-01 19:16:27 -0300

    fix: adjusting enum type

--- app/Enums/PanelTypeEnum.php
+++ app/Enums/PanelTypeEnum.php
@@ -6,14 +6,25 @@ namespace App\Enums;
 
 enum PanelTypeEnum: string
 {
-    case ADMIN = 'admin';
-    case APP = 'app';
+case ADMIN  =   "admin";
+case APP    =   "app";
 
-    public function getLabels(): string
+
+
+    public function getLabel(): ?string
     {
-        return match ($this){
-            self::ADMIN => 'admin',
-            self::APP   => 'APP'
+        return match ($this) {
+            self::ADMIN => 'Administrador',
+            self::APP => 'Usuário',
+        };
+    }
+
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::ADMIN => 'success',
+            self::APP => 'warning',
         };
     }
 }
