69efe3ea9457067b50efddd14fe9a96139883197
Tio Jobs <icarojobsoficial@gmail.com>
2024-03-19 07:55:49 -0300

    typo

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+// [POST] https://freezercontrol.com.br/api/asaas/webhook
+// [POST] http://laravel.test/api/asaas/webhook
+//Route::post('/asaas/webhook', AsaasWehookController::class);
