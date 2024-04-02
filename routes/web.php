e84bf565b7663515c7222a89f5b294030c41a240
Tio Jobs <icarojobsoficial@gmail.com>
2024-04-02 07:39:03 -0300

    fix: fixed default routes

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
-declare(strict_types=1);
-
-use App\Console\Commands\NormalizeCustomerIdCommand;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $panelID = Filament\Facades\Filament::getPanel()->getId();
-
-    return to_route("filament.{$panelID}.pages.dashboard");
+    return to_route('frontend.qr-code');
 })->name('frontend.home');
 
 Route::get('/qr-code', App\Http\Controllers\Frontend\QrCodeController::class)
     ->name('frontend.qr-code');
-
-Route::get('/teste', function () {
-    (new NormalizeCustomerIdCommand())->handle();
-});
 
 Route::fallback(function () {
     return redirect('/app');
