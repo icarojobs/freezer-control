2fffc74af0935a619c0d832230b53e9a25a4e1b3
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-04-03 11:41:25 -0300

    :lipstick: feat: Estilizaçao landing page qr-code

--- resources/views/qrcodes/login.blade.php
+++ resources/views/qrcodes/login.blade.php
@@ -1,19 +1,80 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
+    {{--  --}}
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Acesse Agora</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--LOGO ICON -->
+    <link rel="icon" href="{{asset('images/brands/icon-340.png')}}">
+    <title>{{env('app_name')}}</title>
+    <meta name="description" content="Sistema de gestão de freezers - Revolucionando a forma como você compra bebidas">
+
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link href="{{asset('css/style-qr-code.css')}}" rel="stylesheet">
 </head>
-<body class="bg-gradient-to-r from-blue-300 to-blue-500">
-    <div class="w-1/3 flex flex-col mx-auto p-8 gap-4 bg-blue-100 justify-center items-center mt-16 rounded-xl shadow-lg shadow-gray-500/40">
-        <h1 class="text-3xl">{{ config('app.name') }}</h1>
-        <p class="-mt-4">Revolucionando a forma como você compra bebidas</p>
-        {{ $qrcode }}
-        <a href="{{ route('filament.app.auth.login') }}" class="text-2xl text-blue-600">Acesse Agora</a>
+
+<body class="leading-normal tracking-normal text-gray-900" style="font-family: 'Source Sans Pro', sans-serif;">
+<div class="h-screen pb-14 bg-right bg-cover" style="background-image:url({{asset('images/landing-page/bg.svg')}});">
+    <!--Nav-->
+    <div class="w-full container mx-auto p-6">
+
+        <div class="w-full flex items-center justify-between">
+            <a class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl logo"  href="#">
+                <img src="{{asset('images/brands/icon-340.png')}}" class="mr-3 h-6 sm:h-9" alt="{{env('APP_NAME')}}"/> {{env('APP_NAME')}}
+            </a>
+
+            <div class="flex w-1/2 justify-end content-center">
+                <a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4" data-tippy-content="@twitter_handle"
+                   href="https://www.youtube.com/@TioJobs" target="_blank">
+                    <svg class="fill-current h-7" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><path d="M14.712 4.633a1.754 1.754 0 00-1.234-1.234C12.382 3.11 8 3.11 8 3.11s-4.382 0-5.478.289c-.6.161-1.072.634-1.234 1.234C1 5.728 1 8 1 8s0 2.283.288 3.367c.162.6.635 1.073 1.234 1.234C3.618 12.89 8 12.89 8 12.89s4.382 0 5.478-.289a1.754 1.754 0 001.234-1.234C15 10.272 15 8 15 8s0-2.272-.288-3.367z"/><path fill="#ffffff" d="M6.593 10.11l3.644-2.098-3.644-2.11v4.208z"/></svg>
+                </a>
+                <a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 " data-tippy-content="#facebook_id"
+                   href="https://www.instagram.com/tiojobsoficial/" target="_blank">
+                    <svg class="fill-current h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.9962 0.0078125C8.73824 0.0078125 8.32971 0.021622 7.05019 0.080003C5.77333 0.138241 4.90129 0.341051 4.13824 0.637622C3.34938 0.944146 2.68038 1.35434 2.01343 2.02124C1.34652 2.68819 0.936333 3.35719 0.629809 4.14605C0.333238 4.9091 0.130429 5.78115 0.0721905 7.058C0.0138095 8.33753 0 8.74605 0 12.0041C0 15.262 0.0138095 15.6705 0.0721905 16.9501C0.130429 18.2269 0.333238 19.099 0.629809 19.862C0.936333 20.6509 1.34652 21.3199 2.01343 21.9868C2.68038 22.6537 3.34938 23.0639 4.13824 23.3705C4.90129 23.667 5.77333 23.8698 7.05019 23.9281C8.32971 23.9864 8.73824 24.0002 11.9962 24.0002C15.2542 24.0002 15.6627 23.9864 16.9422 23.9281C18.2191 23.8698 19.0911 23.667 19.8542 23.3705C20.643 23.0639 21.312 22.6537 21.979 21.9868C22.6459 21.3199 23.0561 20.6509 23.3627 19.862C23.6592 19.099 23.862 18.2269 23.9202 16.9501C23.9786 15.6705 23.9924 15.262 23.9924 12.0041C23.9924 8.74605 23.9786 8.33753 23.9202 7.058C23.862 5.78115 23.6592 4.9091 23.3627 4.14605C23.0561 3.35719 22.6459 2.68819 21.979 2.02124C21.312 1.35434 20.643 0.944146 19.8542 0.637622C19.0911 0.341051 18.2191 0.138241 16.9422 0.080003C15.6627 0.021622 15.2542 0.0078125 11.9962 0.0078125ZM11.9962 2.16929C15.1993 2.16929 15.5788 2.18153 16.8437 2.23924C18.0133 2.29257 18.6485 2.488 19.0712 2.65229C19.6312 2.86991 20.0308 3.12986 20.4506 3.54967C20.8704 3.96943 21.1303 4.36905 21.348 4.929C21.5122 5.35172 21.7077 5.98691 21.761 7.15653C21.8187 8.42148 21.831 8.80091 21.831 12.0041C21.831 15.2071 21.8187 15.5866 21.761 16.8515C21.7077 18.0211 21.5122 18.6563 21.348 19.0791C21.1303 19.639 20.8704 20.0386 20.4506 20.4584C20.0308 20.8782 19.6312 21.1381 19.0712 21.3558C18.6485 21.5201 18.0133 21.7155 16.8437 21.7688C15.579 21.8265 15.1996 21.8388 11.9962 21.8388C8.79286 21.8388 8.41352 21.8265 7.14871 21.7688C5.97909 21.7155 5.3439 21.5201 4.92119 21.3558C4.36124 21.1381 3.96162 20.8782 3.54186 20.4584C3.1221 20.0386 2.8621 19.639 2.64448 19.0791C2.48019 18.6563 2.28476 18.0211 2.23143 16.8515C2.17371 15.5866 2.16148 15.2071 2.16148 12.0041C2.16148 8.80091 2.17371 8.42148 2.23143 7.15653C2.28476 5.98691 2.48019 5.35172 2.64448 4.929C2.8621 4.36905 3.12205 3.96943 3.54186 3.54967C3.96162 3.12986 4.36124 2.86991 4.92119 2.65229C5.3439 2.488 5.97909 2.29257 7.14871 2.23924C8.41367 2.18153 8.7931 2.16929 11.9962 2.16929ZM11.9962 16.0028C9.78776 16.0028 7.99748 14.2125 7.99748 12.0041C7.99748 9.79558 9.78776 8.00529 11.9962 8.00529C14.2047 8.00529 15.995 9.79558 15.995 12.0041C15.995 14.2125 14.2047 16.0028 11.9962 16.0028ZM11.9962 5.84381C8.594 5.84381 5.836 8.60181 5.836 12.0041C5.836 15.4062 8.594 18.1642 11.9962 18.1642C15.3984 18.1642 18.1564 15.4062 18.1564 12.0041C18.1564 8.60181 15.3984 5.84381 11.9962 5.84381ZM18.3998 7.03996C19.1949 7.03996 19.8394 6.39548 19.8394 5.60043C19.8394 4.80538 19.1949 4.16086 18.3998 4.16086C17.6048 4.16086 16.9603 4.80538 16.9603 5.60043C16.9603 6.39548 17.6048 7.03996 18.3998 7.03996Z"/></svg>
+                </a>
+            </div>
+
+        </div>
+
     </div>
+
+    <!--Main-->
+    <div class="container pt-24 md:pt-48 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+
+        <!--Left Col-->
+        <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden description">
+            <h1 class="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Revolucionando a forma como você compra bebidas</h1>
+            <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Acesse agora e aprofeitas nossas bebidas!!</p>
+
+            <a href="{{ route('filament.app.auth.login') }}" type="button" class="button text-2xl text-blue-600 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Acesse agora!
+            </a>
+
+        </div>
+
+        <!--Right Col-->
+        <div class="w-full xl:w-3/5 py-0 overflow-y-hidden">
+            <div class="qr-code-container qr-code">
+                {!! html_entity_decode($qrcode) !!}
+            </div>
+        </div>
+
+
+        <!--Footer-->
+        <div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+            <a class="text-gray-500 no-underline hover:no-underline" href="#">{{env('app_name')}} &copy; App {{date('Y')}}</a>
+        </div>
+
+    </div>
+
+
+</div>
+    <!--=======================  SCROLLREVEAL  =======================-->
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <!--=======================  JS  =======================-->
+    <script type="text/javascript" rel="script" src="{{asset('js/script-scrollreveal.js')}}"></script>
 </body>
+
 </html>
