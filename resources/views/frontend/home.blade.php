352e19c825066061ce177e247ded20217d18f409
Jèfferson Gonçalves <411493+jeffersonsimaogoncalves@users.noreply.github.com>
2024-04-04 08:01:41 -0300

    Delete resources/views/frontend/home.blade.php

--- resources/views/frontend/home.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Faça o seu cadastro!</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad atque, cumque debitis dolor eaque exercitationem hic incidunt libero molestiae perferendis placeat porro quae, quas sequi sint sunt totam voluptates.</p>
