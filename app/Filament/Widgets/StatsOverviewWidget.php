8b009e463abfbf97e73e65eb6e99cc951d70e0b6
Wesley Souza <wesleysouza.dev@gmail.com>
2024-04-08 01:43:23 -0300

    feat: adding stats overview

--- app/Filament/Widgets/StatsOverviewWidget.php
+++ app/Filament/Widgets/StatsOverviewWidget.php
@@ -2,16 +2,9 @@
 
 namespace App\Filament\Widgets;
 
-use Carbon\Carbon;
-use App\Models\ProductTransaction;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use Illuminate\Support\Number;
-use Illuminate\Support\Facades\DB;
-use App\Enums\OrderStatusEnum;
-use App\Models\Order;
-use App\Models\Product;
 use App\Services\Stats\StatsControlPanelService;
 use App\Enums\ProductTransactionTypeEnum;
 
@@ -23,105 +16,28 @@ class StatsOverviewWidget extends BaseWidget
 
     protected function getStats(): array
     {
-        $productTransactions = StatsControlPanelService::getProductTransactions();
-        $totalEarningsByNavSales = StatsControlPanelService::getTotalEarningsByNavSales();
-        $totalEarnings = StatsControlPanelService::calculateTotalEarnings($totalEarningsByNavSales);
+        $productTransactions = StatsControlPanelService::getTotalProductTransactions();
         $totalSales = StatsControlPanelService::getTotalSales();
         $totalPurchases = StatsControlPanelService::getTotalPurchases(ProductTransactionTypeEnum::BUY->value);
 
-        $totalEarningsLastDays = $this->getTotalLastDays(StatsControlPanelService::getTotalEarningsLastDays(), 30, true);
-        $totalSalesLastSevenDays = $this->getTotalLastDays(StatsControlPanelService::getTotalSalesLastSevenDays());
-        $totalPurchasesLastSevenDays = $this->getTotalLastDays(StatsControlPanelService::getTotalPurchasesLastDays(), 30);
-
-
-
-        $totalSalesFormatted = formatCurrency($productTransactions->totalSales);
-        $totalPurchasesFormatted = formatCurrency($productTransactions->totalPurchases);
-        $totalEarningsFormatted = formatCurrency($productTransactions->totalEarnings + $totalEarnings);
+        $totalSalesFormatted = formatCurrency($productTransactions['totalSales']);
+        $totalPurchasesFormatted = formatCurrency($productTransactions['totalPurchases']);
+        $totalEarningsFormatted = formatCurrency($productTransactions['totalEarnings']);
+        $percentEarnings = sprintf("%.2f", ($productTransactions['totalEarnings'] / $productTransactions['totalSales']) * 100);
 
         return [
             Stat::make('Total em Vendas (R$)', $totalSalesFormatted)
                 ->description($totalSales . ' vendas')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('warning')
-                ->chart($totalSalesLastSevenDays),
+                ->descriptionIcon('heroicon-m-arrow-up-right')
+                ->color('warning'),
             Stat::make('Total em Compras (R$)', $totalPurchasesFormatted)
                 ->description($totalPurchases . ' compras')
-                ->descriptionIcon('heroicon-m-arrow-trending-down')
-                ->chart($totalPurchasesLastSevenDays) //[7, 2, 10, 3, 15, 4, 17]
+                ->descriptionIcon('heroicon-m-arrow-down-right')
                 ->color('danger'),
             Stat::make('Ganhos Totais (R$)', $totalEarningsFormatted)
-                ->description('3% increase')
+                ->description($percentEarnings . '% de lucro')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart($totalEarningsLastDays)
                 ->color('success'),
         ];
-    }
-
-    public static function getTotalLastDays(array $groupTotal, $subDays = 7, $earnings = false): array
-    {
-        $startDate = now()->subDays($subDays)->startOfDay();
-        $endDate = now()->endOfDay();
-
-        $collection = collect($groupTotal);
-
-        if ($earnings) {
-            $resultOrders = [];
-            foreach ($groupTotal['orders_transactions'] as $orders) {
-                $created_at = Carbon::parse($orders['created_at'])->toDateString();
-                foreach ($orders['items'] as $key => $order) {
-                    $resultOrders[$key]['cost_price'] = (int) $order['quantity'] * $order['cost_price'];
-                    $resultOrders[$key]['sale_price'] = (float) $order['sub_total'];
-                    $resultOrders[$key]['date'] = $created_at;
-                }
-            }
-
-            $orders = collect($resultOrders)->groupBy('date')->map(function ($items, $date) {
-                return [
-                    'date' => $items->first()['date'],
-                    'total_sales' => $items->sum('sale_price') - $items->sum('cost_price')
-                ];
-            })->values()->toArray();
-
-            $product = collect($collection['product_transactions'])->groupBy('date')->map(function ($items, $date) {
-                return [
-                    'date' => $items->first()['date'],
-                    'total_sales' => ($items->first()['quantity'] * $items->sum('sale_price')) - ($items->first()['quantity'] * $items->sum('cost_price'))
-                ];
-            })->values()->toArray();
-
-            $groupTotal = array_merge($orders, $product);
-            usort($groupTotal, function ($a, $b) {
-                return strtotime($a['date']) - strtotime($b['date']);
-            });
-        } else {
-            $groupTotal = $collection->groupBy('date')->map(function ($items, $date) {
-                return [
-                    'date' => $items->first()['date'],
-                    'total_sales' => $items->sum('total_sales')
-                ];
-            })->values()->toArray();
-        }
-
-        $results = [];
-
-        for ($date = $startDate->copy(); $date <= $endDate; $date->addDay()) {
-            $formattedDate = $date->toDateString();
-            $searchIndex = array_search($formattedDate, array_column($groupTotal, "date"));
-
-            if ($searchIndex !== false) {
-                $results[$formattedDate] = $groupTotal[$searchIndex]['total_sales'];
-            } else {
-                $results[$formattedDate] = 0;
-            }
-        }
-
-        if (count($results) > 7) {
-            // array_shift($results);
-        }
-
-        // dd(collect($results)->values()->toArray());
-        // dd(array_sum(collect($results)->values()->toArray()));
-        return collect($results)->values()->toArray();
     }
 }
--- app/Services/Stats/StatsControlPanelService.php
+++ app/Services/Stats/StatsControlPanelService.php
@@ -8,42 +8,74 @@ use App\Enums\OrderStatusEnum;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use App\Enums\ProductTransactionTypeEnum;
-use Carbon\Carbon;
-
-use function PHPSTORM_META\map;
 
 class StatsControlPanelService
 {
-    public static function getProductTransactions(): ?ProductTransaction
+    public static function getTotalProductTransactions(): ?array
     {
-
-        $query = ProductTransaction::with('product')->select(['product_id', 'type', 'quantity'])->where('type', 'sale')->get();
-
-        $result = $query->map(function ($item) {
-            return $item->quantity * $item->product->sale_price;
-        });
-
-        $totalSalesProductTransaction = array_sum($result->toArray());
-
-        $queryOrders = Order::selectRaw('GROUP_CONCAT(items) as items, DATE(created_at) as created_date')
-            ->where('status', OrderStatusEnum::PAID->value)
-            ->groupBy('created_date')
-            ->get();
-
-        dd($queryOrders);
-        $resultOrders = $queryOrders->map(function ($item) {
-            return $item->quantity * $item->product->sale_price;
-        });
-        dd($resultOrders);
-
-
         $query = ProductTransaction::selectRaw('
-        SUM(CASE WHEN type = "sale" THEN (quantity * (SELECT sale_price FROM products WHERE products.id = product_transactions.product_id)) ELSE 0 END) AS totalSales, 
-        SUM(CASE WHEN type = "buy" THEN quantity * (SELECT cost_price FROM products WHERE products.id = product_transactions.product_id) ELSE 0 END) AS totalPurchases,
-        SUM(CASE WHEN type = "sale" THEN (quantity * (SELECT sale_price FROM products WHERE products.id = product_transactions.product_id)) - (quantity * (SELECT cost_price FROM products WHERE products.id = product_transactions.product_id)) ELSE 0 END) AS totalEarnings
+        SUM(
+            CASE 
+                WHEN type = "sale" THEN 
+                    (quantity * (
+                        SELECT sale_price 
+                        FROM products 
+                        WHERE products.id = product_transactions.product_id
+                    )) 
+                    + COALESCE(
+                        (SELECT SUM(total) FROM orders WHERE status = "PAID"), 
+                        0
+                    ) 
+                ELSE 
+                    0 
+            END
+        ) AS totalSales,
+        SUM(
+            CASE
+                WHEN type = "buy" THEN
+                (quantity * (
+                    SELECT cost_price 
+                    FROM products 
+                    WHERE products.id = product_transactions.product_id
+                )) 
+            END
+        ) AS totalPurchases,
+        SUM(
+            CASE 
+                WHEN type = "sale" THEN 
+                    (quantity * (
+                        SELECT sale_price 
+                        FROM products 
+                        WHERE products.id = product_transactions.product_id
+                    )) 
+                    - (quantity * (
+                        SELECT cost_price 
+                        FROM products 
+                        WHERE products.id = product_transactions.product_id
+                    )) 
+                ELSE 
+                    0 
+            END
+        ) AS totalEarnings
         ')->first();
-        dd($query);
-        return $query;
+
+
+        $queryEarningsOrder = Order::select('items')->where('status', OrderStatusEnum::PAID->value)->pluck('items');
+        $totalEarningsOrders = 0;
+
+        $queryEarningsOrder->each(function ($items) use (&$totalEarningsOrders) {
+            foreach ($items as $item) {
+                if (isset($item['cost_price'])) {
+                    $totalEarningsOrders += ($item['quantity'] * $item['unit_price']) - ($item['quantity'] * $item['cost_price']);
+                }
+            }
+        });
+
+        return [
+            'totalSales' => $query->totalSales,
+            'totalPurchases' => $query->totalPurchases,
+            'totalEarnings' => $query->totalEarnings + $totalEarningsOrders,
+        ];
     }
 
     public static function getTotalEarningsByNavSales(): Collection
@@ -73,83 +105,5 @@ class StatsControlPanelService
             ->first()->toArray();
 
         return array_sum($query);
-    }
-
-    public static function getTotalSalesLastSevenDays(): array
-    {
-        $startDate = now()->subDays(7)->startOfDay();
-        $endDate = now()->endOfDay();
-
-        $totalSalesByDayTransaction = ProductTransaction::selectRaw('DATE(created_at) as date, COUNT(*) as total_sales')
-            ->whereBetween('created_at', [$startDate, $endDate])
-            ->where('type', ProductTransactionTypeEnum::SALE->value)
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get()
-            ->toArray();
-
-        $totalSalesByDayOrders = Order::selectRaw('DATE(created_at) as date, COUNT(*) as total_sales')
-            ->whereBetween('created_at', [$startDate, $endDate])
-            ->where('status', OrderStatusEnum::PAID->value)
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get()
-            ->toArray();
-
-        return array_merge($totalSalesByDayOrders, $totalSalesByDayTransaction);
-    }
-
-    public static function getTotalPurchasesLastDays(): array
-    {
-        $startDate = now()->subDays(7)->startOfDay();
-        $endDate = now()->endOfDay();
-
-        $totalSalesByDayTransaction = ProductTransaction::selectRaw('DATE(created_at) as date, COUNT(*) as total_sales')
-            ->whereBetween('created_at', [$startDate, $endDate])
-            ->where('type', ProductTransactionTypeEnum::BUY->value)
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get()
-            ->toArray();
-
-        return $totalSalesByDayTransaction;
-    }
-
-    public static function getTotalEarningsLastDays(): array
-    {
-        $startDate = now()->subDays(30)->startOfDay();
-        $endDate = now()->endOfDay();
-
-        $totalSalesByDayTransaction = ProductTransaction::selectRaw('DATE(product_transactions.created_at) as date, quantity, products.cost_price, products.sale_price')
-            ->join('products', 'product_id', 'products.id')
-            ->whereBetween('product_transactions.created_at', [$startDate, $endDate])
-            ->where('type', ProductTransactionTypeEnum::SALE->value)
-            ->orderBy('date')
-            ->get()
-            ->toArray();
-
-        $totalSalesByDayTransactionOrders = Order::select('items', 'total', 'created_at')
-            ->whereBetween('created_at', [$startDate, $endDate])
-            ->where('status', OrderStatusEnum::PAID->value)
-            ->orderBy('created_at')
-            ->get()
-            ->toArray();
-
-        // return $totalSalesByDayTransactionOrders;
-
-        return ['product_transactions' => $totalSalesByDayTransaction, 'orders_transactions' => $totalSalesByDayTransactionOrders];
-
-        // return array_merge($totalSalesByDayTransaction, $totalSalesByDayTransactionOrders);
-    }
-
-    public static function calculateTotalEarnings(Collection $totalEarningsByNavSales): float
-    {
-        return $totalEarningsByNavSales->map(function ($item) {
-            $quantity = (int) $item['quantity'];
-            $unitPrice = $item['unit_price'];
-            $costPrice = $item['cost_price'];
-
-            return ($quantity * $unitPrice) - ($quantity * $costPrice);
-        })->sum();
     }
 }
