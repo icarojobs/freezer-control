3fff70430893740878840564443c48beb2010076
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-04-12 17:53:24 -0300

    :bug: fix: ajuste createCustomer and UserCreate duplicação 1062

--- app/Filament/Resources/CustomerResource/Pages/CreateCustomer.php
+++ app/Filament/Resources/CustomerResource/Pages/CreateCustomer.php
@@ -13,48 +13,24 @@ class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
 
-//    protected function beforeCreate(): void
-//    {
-//        $existingCustomer = Customer::where('email', $this->data['email'])
-//            ->orWhere('document', sanitize($this->data['document']))
-//            ->first();
-//        if ($existingCustomer) {
-//            if ($existingCustomer->email === $this->data['email'] || $existingCustomer->document === sanitize($this->data['document'])) {
-//                Notification::make('register_error')
-//                    ->title('Cadastro invalido!')
-//                    ->body('Seu E-mail ou CPF são invalidos!')
-//                    ->danger()
-//                    ->persistent()
-//                    ->send();
-//
-//                $this->halt();
-//            }
-//        }
-//    }
+    protected function beforeCreate(): void
+    {
 
-//    protected function beforeFill(): void
-//    {
-//        Notification::make('register_error')
-//            ->title('Cadastro invalido!')
-//            ->body('Seu E-mail ou CPF são invalidos!')
-//            ->danger()
-//            ->persistent()
-//            ->send();
-//    }
-//
-//    protected function afterFill(): void
-//    {
-//        dd('aqui', $this);
-//    }
-//
-//    protected function beforeValidate(): void
-//    {
-//        // É executado antes dos campos do formulário serem validados quando o formulário é enviado.
-//        dd('beforeValidate');
-//    }
-//
-//    protected function afterValidate(): void
-//    {
-//        // É executado depois que os campos do formulário são validados quando o formulário é enviado.
-//    }
+        $existingCustomer = Customer::where('email', $this->data['email'])
+            ->orWhere('document', sanitize($this->data['document']))
+            ->first();
+
+        if ($existingCustomer) {
+            if ($existingCustomer->email === $this->data['email'] || $existingCustomer->document === sanitize($this->data['document'])) {
+                Notification::make('register_error')
+                    ->title('Cadastro invalido!')
+                    ->body('Seu E-mail ou CPF são invalidos!')
+                    ->danger()
+                    ->persistent()
+                    ->send();
+
+                $this->halt();
+            }
+        }
+    }
 }
--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -5,9 +5,32 @@ declare(strict_types=1);
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+
+    protected function beforeCreate(): void
+    {
+
+        $existingCustomer = User::where('email', $this->data['email'])
+            ->first();
+
+        if ($existingCustomer) {
+            if ($existingCustomer->email === $this->data['email']) {
+                Notification::make('register_error')
+                    ->title('Cadastro invalido!')
+                    ->body('Seu E-mail já foi registrado!')
+                    ->danger()
+                    ->persistent()
+                    ->send();
+
+                $this->halt();
+            }
+        }
+    }
 }
