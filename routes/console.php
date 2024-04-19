f623a7ff91f6f4efd78f34e4f52ac43d78c650cf
Fabiano Fernandes <contato@fabianofernandes.adm.br>
2024-04-19 15:11:40 -0300

    wip: alterado mensagem de notificacao de usuario ja registrado

--- routes/console.php
+++ routes/console.php
@@ -8,41 +8,41 @@ use App\Services\PaymentGateway\Gateway;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('play', function () {
-//    $chargeId = 'pay_qeh5d7oe2s1odisx';
-//
-//    $adapter = app(AsaasConnector::class);
-//
-//    $gateway = new Gateway($adapter);
+
+    $adapter = app(AsaasConnector::class);
+
+    $gateway = new Gateway($adapter);
+
+    $customers = $gateway->customer()->list();
+    dd($customers);
 
     // Criar uma nova cobrança
-//    $data = [
-//        "billingType" => "PIX", // "CREDIT_CARD", "PIX", "BOLETO
-//        "discount" => [
-//            "value" => 10,
-//            "dueDateLimitDays" => 0
-//        ],
-//        "interest" => [
-//            "value" => 2
-//        ],
-//        "fine" => [
-//            "value" => 1
-//        ],
-//        "customer" => "cus_000005908684",
-//        "dueDate" => "2024-03-27",
-//        "value" => 100,
-//        "description" => "Pedido 056984",
-//        "daysAfterDueDateToCancellationRegistration" => 1,
-//        "externalReference" => "056984",
-//        "postalService" => false
-//    ];
-//
-//    $payment = $gateway->payment()->create($data);
-//
-//    dd($payment);
+    $data = [
+        "billingType" => "PIX", // "CREDIT_CARD", "PIX", "BOLETO
+        "discount" => [
+            "value" => 10,
+            "dueDateLimitDays" => 0
+        ],
+        "interest" => [
+            "value" => 2
+        ],
+        "fine" => [
+            "value" => 1
+        ],
+        "customer" => "cus_000005908684",
+        "dueDate" => "2024-03-27",
+        "value" => 100,
+        "description" => "Pedido 056984",
+        "daysAfterDueDateToCancellationRegistration" => 1,
+        "externalReference" => "056984",
+        "postalService" => false
+    ];
 
-    // Retorna a listagem de cobranças
-    $payment = $gateway->payment()->get($chargeId);
+    $payment = $gateway->payment()->create($data);
 
     dd($payment);
+
+    // Retorna a listagem de cobranças
+
 
 });
